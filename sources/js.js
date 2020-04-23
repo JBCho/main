073c73e1b385302cb494e9c243a48c3a57d5a101
unknown <jaycho00@postech.ac.kr>
2020-04-23 20:46:59 +0900

    mobile page update js

--- sources/js.js
+++ sources/js.js
@@ -2,9 +2,12 @@ $(document).ready(function(){
     var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
     
     if(isMobile) {
+        var height = window.innerHeight;
+        var name_margintop = height*0.3;
     $("#pagetitle").css({height:window.innerHeight});
-    $("#pagetitle #nametitle").css({margin-top:window.innerHeight*0.3});
+    $("#pagetitle #nametitle").css("margin-top", name_margintop);
     }
+    
     $("#nametitle").fadeIn(1250);
     $("#subtitle").fadeIn(1250);
 });
