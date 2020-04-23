da38fbdef151635888863d58a6efa00d8b45e2ef
unknown <jaycho00@postech.ac.kr>
2020-04-23 19:54:10 +0900

    mobile page update js

--- sources/js.js
+++ sources/js.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    if($("body").hasClass("mobile")) {
+    var viewportHeight = $("#pagetitle").outerHeight();
+    $("#pagetitle").css({height:viewportHeight});
+    }
     $("#nametitle").fadeIn(1250);
     $("#subtitle").fadeIn(1250);
 });
